387b4f2f6b25f6fec6e9ef57b41cf0394bc6e398
Sheeza Aziz <sheeza.aziz@utoronto.ca>
2021-02-10 14:25:41 -0500

    [App.js] removed unneccessary components

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import styled from 'styled-components';
 import Card from './components/Card/Card';
 import NumResults from './components/NumResults/NumResults';
 import SearchInput from './components/SearchInput/SearchInput';
-
 import CardsDisplay from './sections/CardsDisplay/CardsDisplay';
 import LeftMenu from './sections/LeftMenu/LeftMenu';
 import MainPage from './sections/MainPage/MainPage';
-
-
-const cards = [1,2,3,4,5,6];
 
 const AppDisplay = styled.div`
   display: flex;
   height: 100vh;
 `;
 
-function App() {
+export default function App() {
   return (
     <AppDisplay>
       <LeftMenu/>
@@ -25,5 +21,3 @@ function App() {
     </AppDisplay>
   );
 }
-
-export default App;
